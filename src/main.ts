f7510427560c8d9badcf33778fef0a492a0a5b7b
Sergey Sudakov <s3gaddtv@gmail.com>
2023-02-03 17:33:18 +0300

    feat: provide port from .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
+const { PORT = 4000 } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
